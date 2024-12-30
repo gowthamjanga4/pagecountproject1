a240cb81d6f56cd21f758689797acbae76991e25
gowtham <gowthamjanga7@gmail.com>
2024-12-30 19:53:49 +0530

    pushing the project to master branch

---
+++ pagecountproject/testapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def page_count_view(request):
+    print('cookies from the client:', request.COOKIES)
+    count = int(request.COOKIES.get('count', 0))
+    count = count + 1
+    resp = render(request, 'testapp/count.html', {'count': count})
+    resp.set_cookie('count', count)
+    return resp
